883389924209d0700ae1f0fce9841a83c151740e
Jared Yslas <jryslas@gmail.com>
2019-03-07 19:58:02 -0800

    saved changes to Post.js and api posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const Post = require("../../models/Post");
 
 // @route   GET api/posts/test
 // @desc    Tests posts Route
 // @access  Public
 
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+// @route   Post api/posts
+// @desc    Create post
+// @access  Private
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.name,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
