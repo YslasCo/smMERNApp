3e760f6082c658aac7d0316336a46149bbb469e5
Jared Yslas <jryslas@gmail.com>
2019-03-15 16:32:32 -0700

    components:landing, navbar, footer

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <footer className="bg-dark text-white met-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} Social Media App (MERN Stack)
+    </footer>
+  );
+};
